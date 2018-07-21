6bfc1789b43f23da328f5ec3c64abc3bc67555cd
songjiachao <songjiachao@weshare.com.cn>
2018-07-21 16:58:29 +0800

    添加分类

--- src/pages/category/index.js
+++ src/pages/category/index.js
@@ -1,27 +1,174 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Button } from '@tarojs/components'
+import { View, Image, Text } from '@tarojs/components'
 
 import './index.less'
 
 class Index extends Component {
   config = {
-    navigationBarTitleText: '首页'
+    navigationBarTitleText: '分类'
   }
 
-  componentWillReceiveProps (nextProps) {
+  constructor() {
+    super(...arguments)
+    this.state = {
+      selectIndex: '001',
+      data: {
+        '001': {
+          name: '水果',
+          data: [
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '橙子',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+          ]
+        },
+        '002': {
+          name: '生鲜',
+          data: [
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '海带',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+            {
+              code: '001000',
+              name: '全部',
+              image: 'https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg'
+            },
+          ]
+        }
+      }
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
     console.log(this.props, nextProps)
   }
 
-  componentWillUnmount () { }
+  componentWillUnmount() {
+  }
 
-  componentDidShow () { }
+  componentDidShow() {
+  }
 
-  componentDidHide () { }
+  componentDidHide() {
+  }
 
-  render () {
+  changeSelect = (value) => {
+    this.setState({
+      selectIndex: value
+    })
+  }
+
+  render() {
+    const {selectIndex, data} = this.state
     return (
-      <View className='index'>
-        <View>Hello, World</View>
+      <View className='category'>
+        <View className='category-left'>
+          {Object.keys(data).map(v => (
+            <View
+              key={v}
+              className={'left-item' + (selectIndex === v ? ' left-item-active' : '')}
+              onClick={this.changeSelect.bind(this,v)}
+            >
+              {data[v].name}
+            </View>
+          ))}
+        </View>
+        <View className='category-right'>
+          {data[selectIndex].data.map(v => (
+            <View className='right-item'>
+              <Image className='item-image' src={v.image} />
+              <Text className='item-name'>{v.name}</Text>
+            </View>
+          ))}
+        </View>
       </View>
     )
   }
