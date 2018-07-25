1ecc66cf49c895ce5f2d883bb29149f0d3cb747f
feitingting <feitingting@qianmi.com>
2018-07-25 22:26:30 +0800

    购物车页面

--- src/pages/car/index.js
+++ src/pages/car/index.js
@@ -21,7 +21,7 @@ class Index extends Component {
   render() {
     return (
       <View className='index vertical-center'>
-        <View className="clock-container vertical-center">
+        <View className="clock-container">
           <Text className="clock-text">全场满30元包邮</Text>
         </View>
         <View className="cart-tip-container vertical-center">
@@ -30,23 +30,51 @@ class Index extends Component {
           </View>
         </View>
         <View className='goods-container'>
+          <View className='goods-choose'>
+            <Checkbox checked={true} />
+          </View>
           <View className="cart-item">
-            <Checkbox checked={true} className='goods-choose'/>
             <Image className='goods-image' src="https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg">
             </Image>
           </View>
-          <View>
           <View className='goods-content'>
-             <Text className='goods-name'>进口牛油果500g/4g</Text>
-             
-             <View className='goods-bottom'>
-             <Text className='goods-spec'>规格：500g/4个</Text>
-             <Text className='goods-price'>￥20.00</Text>
-             </View>
-          
+            <Text className='goods-name'>进口牛油果500g/4个</Text>
+            <Text className='goods-spec'>规格：500g/4个</Text>
+            <View className='goods-bottom'>
+              <Text className='goods-price'>￥20.00</Text>
+              <Text className='goods-count'>x1</Text>
+            </View>
           </View>
+        </View>
+        <View className='goods-container'>
+          <View className='goods-choose'>
+            <Checkbox checked={true} />
           </View>
-     
+          <View className="cart-item">
+            <Image className='goods-image' src="https://www.fruitzj.com/uploads/allimg/180604/4-1P6041H459.jpg">
+            </Image>
+          </View>
+          <View className='goods-content'>
+            <Text className='goods-name'>西红柿西红柿西红柿西红柿西红柿西红柿西红柿西红柿西红柿西红柿西红柿西红柿西红柿</Text>
+            <Text className='goods-spec'>规格：2.0Kg/1箱子</Text>
+            <View className='goods-bottom'>
+              <Text className='goods-price'>￥25.00</Text>
+              <Text className='goods-count'>x10000</Text>
+            </View>
+          </View>
+        </View>
+        <View className='botton-bar'>        
+          <View className='goods-choose'>
+            <Checkbox checked={true} />
+            <Text className='choose-text'>全选</Text>
+          </View>
+          <View className='buy-middle'>
+            <Text className='total-price'>合计:￥25.00</Text>
+            <Text className='delivery-price'>配送费:￥5.00</Text>
+          </View>
+          <View className='buy-botton'>
+            <Text className='buy-text'>去结算</Text>
+          </View>
         </View>
 
       </View>
