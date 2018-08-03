a3d132252d0e32c26d5b578decba179f1f46fbf2
songjiachao <songjiachao@weshare.com.cn>
2018-08-03 10:25:05 +0800

    我的页面添加订单导航

--- src/pages/user/components/order-bar/index.js
+++ src/pages/user/components/order-bar/index.js
@@ -1,11 +1,41 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View  } from '@tarojs/components'
+import { View, Image, Text } from '@tarojs/components'
 import './index.less'
+
+const orderArray = [
+  {
+    title: '全部订单',
+    icon: require('./images/all.png')
+  },
+  {
+    title: '待支付',
+    icon: require('./images/pay.png')
+  },
+  {
+    title: '待收货',
+    icon: require('./images/car.png')
+  },
+  {
+    title: '已完成',
+    icon: require('./images/down.png')
+  },
+  {
+    title: '售后',
+    icon: require('./images/service.png')
+  },
+]
 
 export default class OrderBar extends Component {
   render() {
     return (
-      <View className='order-bar' >1212</View>
+      <View className='order-bar'>
+        {orderArray.map((v, i) => (
+          <View key={i} className='order-bar-item'>
+            <Image className='order-bar-image' src={v.icon} />
+            <Text className='order-bar-text'>{v.title}</Text>
+          </View>
+        ))}
+      </View>
     )
   }
 }
